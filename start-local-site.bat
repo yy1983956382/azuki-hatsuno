b9853135ad7926e955e1f5cca8015f0d304c986b
yy1983956382 <287206533+yy1983956382@users.noreply.github.com>
2026-09-07 03:13:41 +0900

    chore: keep repository limited to website files

--- start-local-site.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0start-local-site.ps1"
--- start-public-site.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0start-local-site.ps1" -BindAddress 0.0.0.0
